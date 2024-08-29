58241c8e37d84f8a5148f284b7f29bdd4a767d0c
DHindsCTaccess <dhinds@ctaccess.com>
2024-08-29 09:19:51 -0500

    Rotated secret

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.addEventListener('load', function () {
 
 function initiateLogin() {
     const clientId = 'b493f960-3a0e-4a02-89f7-b09b9db97a1c';
-    const redirectUri = 'http://127.0.0.1:5501/index.html';
+    const redirectUri = 'https://dhindsctaccess.github.io/SMART_Dashboard/index.html';
     const customerId = '961460947';
     const scope = 'repository.Read';
     const state = generateRandomState(16);
@@ -94,8 +94,8 @@ if (code && returnedState === storedState) {
 // Function to exchange code for access and refresh tokens
 function exchangeCodeForTokens(code) {
     const clientId = 'b493f960-3a0e-4a02-89f7-b09b9db97a1c';
-    const clientSecret = 'IMWG2oT22FVY6kFXh7jnVVk7Ujez82TsRwe9y3OpjCyxXL8G';
-    const redirectUri = 'http://127.0.0.1:5501/index.html';
+    const clientSecret = 'CMoi3M0DVjzOCsXkZGmRg1A2QAyhNpo8IhKU3mafqFVFcUxp';
+    const redirectUri = 'https://dhindsctaccess.github.io/SMART_Dashboard/index.html';
 
     const tokenUrl = 'https://signin.laserfiche.com/oauth/token';
     const auth = btoa(`${clientId}:${clientSecret}`);
@@ -437,7 +437,7 @@ function updateStepProgress(progressElement, value) {
 // Function to refresh access token
 function refreshToken() {
     const clientId = 'b493f960-3a0e-4a02-89f7-b09b9db97a1c';
-    const clientSecret = 'IMWG2oT22FVY6kFXh7jnVVk7Ujez82TsRwe9y3OpjCyxXL8G';
+    const clientSecret = 'CMoi3M0DVjzOCsXkZGmRg1A2QAyhNpo8IhKU3mafqFVFcUxp';
     const refreshToken = sessionStorage.getItem('refresh_token');
 
     const tokenUrl = 'https://signin.laserfiche.com/oauth/token';
