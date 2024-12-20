798f4e365ae7090e6425b9a26e57eaf5624ef5f0
HindsD <dhinds423@gmail.com>
2024-12-20 11:53:44 -0600

    Changing passcode logic

--- passcode.js
+++ passcode.js
@@ -6,13 +6,11 @@ const passcodes = {
     'all456': 'All'
 };
 
-const baseUrl = 'https://dhindsctaccess.github.io/SMART_Dashboard';
-
 document.getElementById('submit-passcode').addEventListener('click', function () {
     const passcode = document.getElementById('passcode').value;
     if (passcodes.hasOwnProperty(passcode)) {
         sessionStorage.setItem('userRole', passcodes[passcode]);
-        window.location.href = `${baseUrl}/index.html`;
+        window.location.href = 'index.html';
     } else {
         alert('Invalid passcode');
     }
