1b085d4f8bc649ffa61a34711650e4ca4f8c7428
HindsD <dhinds423@gmail.com>
2024-12-20 11:49:14 -0600

    Fixing issues with passcode authentication

--- passcode.js
+++ passcode.js
@@ -6,11 +6,13 @@ const passcodes = {
     'all456': 'All'
 };
 
+const baseUrl = 'https://dhindsctaccess.github.io/SMART_Dashboard';
+
 document.getElementById('submit-passcode').addEventListener('click', function () {
     const passcode = document.getElementById('passcode').value;
     if (passcodes.hasOwnProperty(passcode)) {
         sessionStorage.setItem('userRole', passcodes[passcode]);
-        window.location.href = 'index.html';
+        window.location.href = `${baseUrl}/index.html`;
     } else {
         alert('Invalid passcode');
     }
